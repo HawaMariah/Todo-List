fc1ecccc687e2c12e3f6ae6dfb8dc9cf87ee9232
Hawa <hawamaria33@gmail.com>
2024-01-17 16:43:05 +0300

    to do list competed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import TodoList from "./Components/TodoList"
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+  <div>
+    <TodoList/>
+  </div>
+    
+  
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChang(e) {
+    setNewTask(e.target.value);
+  }
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((element, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+  
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+  
+
+  return (
+    <>
+      <div className="to-do-list">
+        <h1>To Do List</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="enter a task .. "
+            value={newTask}
+            onChange={handleInputChang}
+          />
+          <button className="add-btn" onClick={addTask}>
+            Add
+          </button>
+        </div>
+        <ol>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span className="text">{task}</span>
+              <button className="delete-btn" onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+              <button className="move-btn" onClick={() => moveTaskUp(index)}>
+                Up
+              </button>
+              <button className="move-btn" onClick={() => moveTaskDown(index)}>
+                Down
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
+}
+
+export default TodoList;
